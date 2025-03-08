b7657f8d4148193d49f6828209de7ece191eabb8
Jaden243 <144974650+Jaden243@users.noreply.github.com>
2025-03-08 12:17:04 -0500

    Build Settings updated
    took out code in PlayerController Test Script
    
    using static UnityEditor.Timeline.TimelinePlaybackControls;

--- Assets/PlayControllerTest.cs
+++ Assets/PlayControllerTest.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
-using static UnityEditor.Timeline.TimelinePlaybackControls;
+
 
 public class PlayerControllerTest : MonoBehaviour
 {
